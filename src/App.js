a879f641593175a994059b539d11e9607e00dc2d
Aleksey <lesha.sinitsin@mail.ru>
2021-08-20 16:53:12 +0300

    add animation route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { HashRouter, Redirect, Route } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Home from './components/Home/Home';
 import Myworks from './components/Myworks/Myworks';
-
+import { useTransition, animated } from "react-spring";
 function App() {
-  return (
+  let location = useLocation();
+
+  let transitions = useTransition(location, {
+    from: {
+      opacity: 0,
+      transform: "scale(0)",
+      transition: " 1s"
+    },
+    enter: {
+      opacity: 1,
+      transform: "scale(1)",
+      transition: " 1s"
+    },
+    leave: {
+      opacity: 0,
+      transform: "scale(0)",
+      transition: " 1s"
+    }
+  })
+
+  return transitions((props, item) => (
     <div className="App">
-      <HashRouter>
-        <Redirect from="/" to="/Home" />
-        <Route
-          path="/Home"
-          render={() => (<Home />)}
-        />
-        <Route
-          path="/Myworks"
-          component={() => <Myworks />}
-        />
-      </HashRouter>
+      <animated.div style={props} className="animated_div">
+        <Switch location={item}>
+          <Route exact
+            path="/"
+            component={Home}
+          />
+          <Route exact
+            path="/Myworks"
+            component={Myworks}
+          />
+        </Switch>
+      </animated.div>
     </div>
-  );
+  ))
+
 }
+
 
 export default App;
